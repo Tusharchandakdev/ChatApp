3dffc305bc6e4f277fa5b195f3e6c3aab54259e5
Tusharchandakdev <tusharchandak199898@gmail.com>
2023-01-30 15:51:03 +0530

    Done

---
+++ src/main/java/com/chatApp/Controller/MessageController.java
@@ -0,0 +1,36 @@
+package com.chatApp.Controller;
+
+
+import com.chatApp.Models.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+
+
+
+   @MessageMapping("/message")
+   @SendTo("/topic/return-to")
+    public Message getContent(@RequestBody Message message)
+
+    {
+
+        try {
+
+            Thread.sleep(200);
+        }
+        catch(InterruptedException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return message;
+    }
+
+
+}
